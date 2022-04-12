2552a498aec85cd1ec7b0f5c27373f22ae9ee35f
unknown <crsiufung@hotmail.com>
2022-04-12 18:49:13 +0800

    auth user with backend server, set cookies, add logout feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,23 @@ import { ChannelContainer, ChannelListContainer, Auth } from './components';
 
 import "./App.css";
 
+const cookies = new Cookies();
+
 const apiKey = "g8cpznn58mtv";
+const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
 
-const authToken = false;
+if (authToken) {
+  client.connectUser({
+    name: cookies.get("username"),
+    fullName: cookies.get("fullName"),
+    id: cookies.get("userId"),
+    phoneNumber: cookies.get("phoneNumber"),
+    image: cookies.get("avatarURL"),
+    hashedPassword: cookies.get("hashedPassword"),
+  }, authToken)
+}
 
 const App = () => {
 
